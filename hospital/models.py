87614c9c38445f346bdff2bb6055642de4a4bb11
JackloveCheng <106416821+JackloveCheng@users.noreply.github.com>
2023-12-09 23:31:27 +0800

    123“

---
+++ hospital/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+departments=[('fix','fix'),
+
+]
+class Doctor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return "{} {}".format(self.user.first_name,self.user.last_name)
+
+
+
+
+class Patient(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
+    admitDate=models.DateField(auto_now=True)
+    status=models.BooleanField(default=False)
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+
+    def __str__(self):
+        return self.user.first_name + self.user.last_name
+
+
+class Appointment(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    doctorId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40,null=True)
+    doctorName=models.CharField(max_length=40,null=True)
+    appointmentDate=models.DateField(auto_now=True)
+    description=models.TextField(max_length=500)
+    status=models.BooleanField(default=False)
+
+
+
+class PatientDischargeDetails(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40)
+    assignedDoctorName=models.CharField(max_length=40)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    symptoms = models.CharField(max_length=100,null=True)
+
+    admitDate=models.DateField(null=False)
+    releaseDate=models.DateField(null=False)
+    daySpent=models.PositiveIntegerField(null=False)
+
+    roomCharge=models.PositiveIntegerField(null=False)
+    medicineCost=models.PositiveIntegerField(null=False)
+    doctorFee=models.PositiveIntegerField(null=False)
+    OtherCharge=models.PositiveIntegerField(null=False)
+    total=models.PositiveIntegerField(null=False)
+
+
+#Developed By : sumit kumar
+#facebook : fb.com/sumit.luv
+#Youtube :youtube.com/lazycoders
